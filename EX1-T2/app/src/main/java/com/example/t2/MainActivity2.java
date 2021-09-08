69cd7a788bbef4d10228ad945928c637dfe05539
andrei-azevedo <deicordova@hotmail.com>
2021-09-07 23:06:10 -0300

    add ex1-t2

---
+++ EX1-T2/app/src/main/java/com/example/t2/MainActivity2.java
@@ -0,0 +1,18 @@
+package com.example.t2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        TextView textView = (TextView) findViewById(R.id.textViewMessageData);
+        textView.setText(getIntent().getStringExtra("message"));
+    }
+}
